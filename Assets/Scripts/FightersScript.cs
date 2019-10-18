2c68fb4e2d96de775ddbd991d006e283b55586db
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-10-18 09:53:15 -0400

    added new fighter enemy

---
+++ Assets/Scripts/FightersScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightersScript : MonoBehaviour
+{
+    public float speed = 1f;
+    public float distance = 3f;
+    private bool moveRight = true;
+    public Transform groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, distance);
+        if(groundInfo.collider == false)
+        {
+            if(moveRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveRight = true;
+            }
+        }
+    }
+}
